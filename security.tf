78a7d44c7f6286c9c40f846beab9d17204c579da
Ubuntu <ubuntu@ip-172-31-47-226.ap-south-1.compute.internal>
2020-11-22 08:06:05 +0000

    added 8080 port for elb

--- security.tf
+++ security.tf
@@ -35,7 +35,13 @@ resource "aws_security_group" "lb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    description = "HTTP from laptop"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
